e58acb176037fd27516554f262fb979b1803fc2b
xobotyi <xog3@yandex.ru>
2019-05-17 03:32:55 +0300

    removed useless console.log;

--- src/js/app/AppFooter.jsx
+++ src/js/app/AppFooter.jsx
@@ -2,13 +2,10 @@ import * as React from "react";
 
 export default class AppFooter extends React.Component {
   render() {
-    console.log(RSC_HOMEPAGE, RSC_NAME, RSC_AUTHOR);
-
     return (
       <div id="App-Footer">
         <div className="links">
-          <a href={RSC_HOMEPAGE}>{RSC_NAME}</a>×
-          <a href={RSC_AUTHOR.url}>{RSC_AUTHOR.name}</a>
+          <a href={RSC_HOMEPAGE}>{RSC_NAME}</a>×<a href={RSC_AUTHOR.url}>{RSC_AUTHOR.name}</a>
         </div>
       </div>
     );

600328db1a472a5105bf742d00f8f5baf2f558f6
xobotyi <xog3@yandex.ru>
2019-06-28 16:52:33 +0300

    - Now for cjs and esm modules bundled differently; - ESM version now has ESNext lang level; - CJS version now has ES3 lang level;

--- rollup.config.js
+++ rollup.config.js
@@ -2,55 +2,70 @@ import babel from "rollup-plugin-babel";
 import ts from "rollup-plugin-typescript2";
 import pkg from "./package.json";
 
-export default {
-  input: "./src/index.ts",
+const ownKeys = Object.getOwnPropertyNames;
+const externalDependencies = Array.from(new Set(ownKeys(pkg.peerDependencies).concat(ownKeys(pkg.dependencies))));
 
-  output: [
-    {
-      dir: "dist/cjs",
-      extend: true,
-      format: "cjs",
-      exports: "named"
-    },
-    {
-      dir: "dist/esm",
-      extend: true,
-      format: "esm",
-      exports: "named"
-    }
-  ],
-
-  external: ["react", "prop-types", "cnbuilder", "react-draggable", "zoom-level", "is-number", "is-fun"],
-
-  plugins: [
-    ts({
-      clean: true,
-      useTsconfigDeclarationDir: true,
-      tsconfigOverride: {
-        compilerOptions: {
-          module: "esnext",
-          target: "esnext",
-          declaration: true,
-          declarationDir: "dist/types"
-        }
+export default [
+  {
+    input: "./src/index.ts",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+        exports: "named"
       }
-    }),
-    babel({
-      babelrc: false,
-      exclude: "node_modules/**",
-      extensions: [".ts", ".tsx", ".js", ".jsx"],
-      presets: [
-        [
-          "@babel/preset-env",
-          {
-            targets: {
-              esmodules: false,
-              chrome: "52",
-              ie: "10"
-            }
+    ],
+    external: externalDependencies,
+    plugins: [
+      ts({
+        clean: true,
+        tsconfigOverride: {
+          compilerOptions: {
+            module: "esnext",
+            target: "es5",
+            declaration: false
           }
+        }
+      }),
+      babel({
+        babelrc: false,
+        exclude: "node_modules/**",
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+        presets: [
+          [
+            "@babel/preset-env",
+            {
+              targets: {
+                ie: "9"
+              }
+            }
+          ]
         ]
-      ]
-    })
-  ]
-};
+      })
+    ]
+  },
+  {
+    input: "./src/index.ts",
+    output: [
+      {
+        file: pkg.module,
+        format: "esm"
+      }
+    ],
+    external: externalDependencies,
+    plugins: [
+      ts({
+        clean: true,
+        useTsconfigDeclarationDir: true,
+        tsconfigOverride: {
+          compilerOptions: {
+            module: "esnext",
+            target: "esnext",
+            declaration: true,
+            declarationDir: __dirname + "/dist/types"
+          }
+        }
+      })
+    ]
+  }
+];

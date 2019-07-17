c65a9a863aa0443b67709d2817a57cfef34c0629
xobotyi <xog3@yandex.ru>
2019-07-17 12:19:39 +0300

    Props types fix; Removed console log(Fix: #98)

--- src/Scrollbar.tsx
+++ src/Scrollbar.tsx
@@ -743,7 +743,6 @@ export default class Scrollbar extends React.Component<ScrollbarProps, Scrollbar
     }
 
     if (!props.native && bitmask & (1 << 15)) {
-      console.log(getScrollbarWidth(true));
       this.forceUpdate();
     }
 
@@ -827,7 +826,7 @@ export default class Scrollbar extends React.Component<ScrollbarProps, Scrollbar
       children,
 
       ...propsHolderProps
-    } = this.props;
+    } = this.props as ScrollbarProps;
 
     const scrollbarWidth = !isUndef(propsScrollbarWidth) ? propsScrollbarWidth! : util.getScrollbarWidth() || 0;
 

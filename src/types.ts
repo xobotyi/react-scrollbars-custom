9cc845a02ebc8c11c2a75da5f050471804480c60
xobotyi <xog3@yandex.ru>
2022-07-25 10:51:46 +0300

    fix: explicit children typing

--- src/types.ts
+++ src/types.ts
@@ -16,7 +16,9 @@ export type ElementPropsWithElementRef<T = HTMLDivElement> = React.HTMLProps<T> 
   elementRef?: ElementRef<T>;
 };
 
-export type ElementRenderer<T = HTMLDivElement> = React.FC<ElementPropsWithElementRef<T>>;
+export type ElementRenderer<T = HTMLDivElement> = React.FC<
+  React.PropsWithChildren<ElementPropsWithElementRef<T>>
+>;
 
 export type ElementPropsWithElementRefAndRenderer<T = HTMLDivElement> = React.HTMLProps<T> & {
   elementRef?: ElementRef<T>;
